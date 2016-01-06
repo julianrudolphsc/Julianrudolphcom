66f8a862e72a373f7e10ca734fb2f160c7e3a60e
julian rudolph <julian.rudolphsc@gmail.com>
2016-01-06 10:30:54 -0500

    created projects crud
    CRUD functionality on projects

--- julianrudolph/lib/collections.js
+++ julianrudolph/lib/collections.js
@@ -22,3 +22,51 @@ Posts.attachSchema(new SimpleSchema({
     autoValue: function(){ return new Date() }
   }
 }));
+
+//projections collection
+Projects = new Mongo.Collection('projects');
+
+Projects.attachSchema(new SimpleSchema({
+  name: {
+    type: String,
+    max: 100
+  },
+
+  description:{
+    type: String,
+    max: 1000
+  },
+  client: {
+    type: String,
+    max: 100
+  },
+  type:{
+    type: String,
+    max: 200
+  },
+projectDate:{
+  type: String,
+  max: 100,
+  optional: true
+},
+  userId:{
+    type: String,
+    autoValue: function(){ return Meteor.userId() }
+  },
+
+  updatedAt: {
+    type: Date,
+    autoValue: function(){ return new Date() }
+  },
+
+  projectImage:{
+    type: String,
+    max: 200,
+    optional: true
+  }
+}));
+
+//img collection
+ProjectImages = new FS.Collection('ProjectImages', {
+  stores:[new FS.Store.GridFS('ProjectImages')]
+});
